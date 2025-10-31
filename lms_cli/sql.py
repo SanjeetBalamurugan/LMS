2385084bc0f8d1e9d798eace3e608fa056d298f9
SanjeetBalamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-10-31 18:35:55 +0530

    initial setup

---
+++ lms_cli/sql.py
@@ -0,0 +1,8 @@
+import mysql.connector as con
+
+class ClassName(object):
+    """docstring for ClassName."""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+        
